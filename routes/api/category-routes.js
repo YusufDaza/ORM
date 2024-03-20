edf23e0a43000e8fd11fec553509b938add29527
Joseph Daza <josephdazz@hotmail.com>
2024-03-20 14:01:23 -0400

    category-routes.js updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,17 +15,74 @@ router.get('/', async (req, res) =>{
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+//http://localhost:3001/api/categories/1
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.ed, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
-  // create a new category
+//http:localhost:3001/api/categories/
+router.post('/', async (req, res) =>{
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      category_name: req.body_name 
+
+    },{
+      where: {
+        id: req.params.id, 
+      },
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//http://localhost:3001/api/categories/2
+router.delete('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id, 
+      },
+    });
+
+    if(!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
