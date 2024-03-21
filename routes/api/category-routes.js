cb1da3134f31bdda9fed38dd4d552810b9eca2ed
Joseph Daza <josephdazz@hotmail.com>
2024-03-20 20:57:55 -0400

    server.js updated

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,8 +11,8 @@ router.get('/', async (req, res) =>{
     });
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err);
-  }
+    res.status(500).json(err); 
+}
 });
 
 //http://localhost:3001/api/categories/1
