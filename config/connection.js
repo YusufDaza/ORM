e3a17ab9020d16caabfac6c8d302815801e77f55
Joseph Daza <josephdazz@hotmail.com>
2024-03-21 21:40:11 -0400

    tags.js updated

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,9 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
 
 module.exports = sequelize;
